0c6e59a7b1bfcc3927b820a7858b7245f51a94eb
Romeen Gurung <romeenrg@hotmail.com>
2025-12-02 22:23:05 +0000

    Test progress done

--- 03_resources/classes_test.py
+++ 03_resources/classes_test.py
@@ -3,19 +3,19 @@ import os
 class Updater(): 
 
     def __init__(self):
-        self.file_path_allow = "/Users/romeenrg/Projects/Testing_skills/extending-testing-resources/unit2/03_resources/target_directory/allowlist"
-        self.file_path_drop = "/Users/romeenrg/Projects/Testing_skills/extending-testing-resources/unit2/03_resources/target_directory/droplist"
+        self.file_path_allow = "/Users/romeenrg/Projects/Testing_skills/challenge/03_resources/target_directory/allowlist"
+        self.file_path_drop = "/Users/romeenrg/Projects/Testing_skills/challenge/03_resources/target_directory/droplist"
     pass
 
     def add_file_to_originals (self, filename, name, address):
-        file_path_originals = "/Users/romeenrg/Projects/Testing_skills/extending-testing-resources/unit2/03_resources/target_directory/originals/" + filename
+        file_path_originals = "/Users/romeenrg/Projects/Testing_skills/challenge/03_resources/target_directory/originals/" + filename
         with open(file_path_originals, 'w') as file:
             file.write(f"{name}\n")
             file.write(address)
             return
 
     def add_file_to_updates (self, filename, name, address):
-        file_path_updates = "/Users/romeenrg/Projects/Testing_skills/extending-testing-resources/unit2/03_resources/target_directory/updates/" + filename
+        file_path_updates = "/Users/romeenrg/Projects/Testing_skills/challenge/03_resources/target_directory/updates/" + filename
         with open(file_path_updates, 'w') as file:
             file.write(f"{name}\n")
             file.write(address)
